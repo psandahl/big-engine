656845981d55eb0fa9659429376e5d3f4194c36b
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-07-04 14:07:21 +0200

    Reading models from file.

--- src/BigE/Model.hs
+++ src/BigE/Model.hs
@@ -10,23 +10,48 @@
 -- used to create meshes.
 module BigE.Model
     ( vertPFromFile
+    , vertPNFromFile
     , vertPNTxFromFile
     ) where
 
 import qualified BigE.Attribute.Vert_P      as Vert_P
+import qualified BigE.Attribute.Vert_P_N    as Vert_P_N
 import qualified BigE.Attribute.Vert_P_N_Tx as Vert_P_N_Tx
+import           BigE.Model.Assembler       (assembleVertP, assembleVertPN,
+                                             assembleVertPNTx)
+import           BigE.Model.Parser          (FilePart, fromFile)
 import           Control.Monad.IO.Class     (MonadIO)
-import           Data.Vector.Storable       (Vector)
+import           Data.Vector.Storable       (Vector, fromList)
+import           Foreign                    (Storable)
 import           Graphics.GL                (GLuint)
 
 -- | Read a model from the given file. Only use the position attribute. All
 -- valid model files shall be able to read.
 vertPFromFile :: MonadIO m => FilePath
               -> m (Either String (Vector Vert_P.Vertex, Vector GLuint))
-vertPFromFile = undefined
+vertPFromFile = readIt assembleVertP
+
+-- | Read a model from the given file. The model file must have the attributes
+-- position and normal to be able to load.
+vertPNFromFile :: MonadIO m => FilePath
+               -> m (Either String (Vector Vert_P_N.Vertex, Vector GLuint))
+vertPNFromFile = readIt assembleVertPN
 
 -- | Read a model from the given file. The model file must have the attributes
 -- position, normal and texture coordinate to be able to load.
 vertPNTxFromFile :: MonadIO m => FilePath
                  -> m (Either String (Vector Vert_P_N_Tx.Vertex, Vector GLuint))
-vertPNTxFromFile = undefined
+vertPNTxFromFile = readIt assembleVertPNTx
+
+readIt :: (Storable a, Storable b, MonadIO m)
+       => ([FilePart] -> Maybe ([a], [b])) -> FilePath
+       -> m (Either String (Vector a, Vector b))
+readIt assemble file = do
+    eParts <- fromFile file
+    case eParts of
+        Right parts ->
+            case assemble parts of
+                Just (xs, ys) -> return $ Right (fromList xs, fromList ys)
+                Nothing       -> return $ Left "Cannot assemble model parts"
+
+        Left err -> return $ Left err
